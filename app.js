a9882d7a8783bceaede3bb89340beddaa539420b
RAnastasiya <rizhko.anastasiya@gmail.com>
2023-05-10 15:46:59 +0300

    update

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 
 app.get('/tasks', TaskController.findAllTasks)
-app.post('/tasks', TaskController.createTask)
+app.post('/tasks', TaskController.createTask);
+
+app.use((err, req, res, next) => {
+    const status = err.statusCode || 500;
+    const message = err.message || 'Server error';
+    res.status(status).send(message);
+})
 
 module.exports = app;
--- models/Task.js
+++ models/Task.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const taskSchema = new Schema({
-    content: String,
+    content: { 
+        type: String, 
+        required: [true, 'Content number required'],
+        validate: {
+            validator: (v) => /[A-Za-z0-9\s]{5,512}/.test(v),
+            message: props => `${props.value} is not a valid content!`
+        },
+    },
     isDone: { type: Boolean, default: false },
     dateAt: {
         type: Date, 
@@ -10,7 +17,7 @@ const taskSchema = new Schema({
     },
     author: {
         name: String,
-        age: Number
+        age: { type: Number, min: 14, max: 120 }
     }
 })
 const Task = mongoose.model('Task', taskSchema);
